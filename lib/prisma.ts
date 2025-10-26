84265513b5ffc135d1ef0aa24dda527fe1acd781
muhammadakafe <ibusiness.21s@gmail.com>
2025-10-26 17:46:29 +0200

    Remove dotenv configuration from prisma.ts as it is no longer needed, streamlining the database client setup.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import dotenv from 'dotenv';
-dotenv.config();
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 // Check if DATABASE_URL is set
